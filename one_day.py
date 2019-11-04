ed9596c72210555c2907265a684693b06a4392b9
yangjuna <925413593@qq.com>
2019-11-04 15:00:12 +0800

    '添加2019-11-04练习1'

--- one_day.py
+++ one_day.py
@@ -122,8 +122,138 @@
 # print(cc.pop(len(cc) - 1))
 # print(cc.clear())#清空列表
 
-fruits = ['grape', 'apple', 'strawberry', 'waxberry']
-fruits += ['pitaya', 'pear', 'mango']
-# 列表切片
-fruits2 = fruits[1:4]
-print(fruits2)
+# fruits = ['grape', 'apple', 'strawberry', 'waxberry']
+# fruits += ['pitaya', 'pear', 'mango']
+# # 列表切片
+# fruits2 = fruits[1:4]
+# print(fruits2)
+#
+# list1 = ['orange', 'apple', 'zoo', 'internationalization', 'blueberry']
+# list2 = sorted(list1)
+# # sorted函数返回列表排序后的拷贝不会修改传入的列表
+# # 函数的设计就应该像sorted函数一样尽可能不产生副作用
+# list3 = sorted(list1, reverse=True)
+# # 通过key关键字参数指定根据字符串长度进行排序而不是默认的字母表顺序
+# list4 = sorted(list1, key=len)
+# print(list1)
+# print(list2)
+# print(list3)
+# print(list4)
+# # 给列表对象发出排序消息直接在列表对象上进行排序
+# list1.sort(reverse=True)
+# print(list1)
+# #列表生成器
+# f = [x for x in range(1, 10)]
+# print(f)
+# f = [x + y for x in 'ABCDE' for y in '1234567']
+# print(f)
+#
+# import sys
+# f = (x ** 2 for x in range(1, 1000))
+# print(sys.getsizeof(f))
+# for val in f:
+#     print(val)
+
+#yield 斐波那契数列
+# def fib(n):
+#     a, b = 0, 1
+#     for _ in range(n):
+#         a, b = b, a + b
+#         yield a
+#
+#
+# def main():
+#     for val in fib(20):
+#         print(val)
+#
+#
+# if __name__ == '__main__':
+#     main()
+
+
+
+# #元组内容不允许修改，但是可以转换为列表，元组在创建时间和占用的空间上面都优于列表
+# # 定义元组
+# t = ('杨军', 25, True, '山东青岛')
+# print(t)
+# # 获取元组中的元素
+# print(t[0])
+# print(t[3])
+# # 遍历元组中的值
+# for member in t:
+#     print(member)
+# # 重新给元组赋值
+# # t[0] = '王大锤'  # TypeError
+# # 变量t重新引用了新的元组原来的元组将被垃圾回收
+# t = ('王大锤', 20, True, '云南昆明')
+# print(t)
+# # 将元组转换成列表
+# person = list(t)
+# print(person)
+# # 列表是可以修改它的元素的
+# person[0] = '李小龙'
+# person[1] = 25
+# print(person)
+# # 将列表转换成元组
+# fruits_list = ['apple', 'banana', 'orange']
+# fruits_tuple = tuple(fruits_list)
+# print(fruits_tuple)
+
+#
+#
+# #集合不允许有重复数据
+# set1 = {1, 2, 3, 3, 3, 2}
+# print(set1)
+# print('Length =', len(set1))
+# # 创建集合的构造器语法(面向对象部分会进行详细讲解)
+# set2 = set(range(1, 10))
+# set3 = set((1, 2, 3, 3, 2, 1))
+# print(set2, set3)
+# # 创建集合的推导式语法(推导式也可以用于推导集合)
+# set4 = {num for num in range(1, 100) if num % 3 == 0 or num % 5 == 0}
+# print(set4)
+# #象集合中添加元素
+# set1.add(199)
+# print(set1)
+# set2.update([11, 12])
+# print(set2)
+# set2.discard(5)#丢弃
+# print(set2)
+#
+#
+# if 4 in set2:
+#     set2.remove(4)#移除
+# print(set1, set2)
+# print(set3.pop())#弹出第一个元素
+# print(set3)
+#
+#
+# # 集合的交集、并集、差集、对称差运算
+# print(set1 & set2)
+# # print(set1.intersection(set2))
+# print(set1 | set2)
+# # print(set1.union(set2))
+# print(set1 - set2)
+# # print(set1.difference(set2))
+# print(set1 ^ set2)
+# # print(set1.symmetric_difference(set2))
+# # 判断子集和超集
+# print(set2 <= set1)
+# # print(set2.issubset(set1))
+# print(set3 <= set1)
+# # print(set3.issubset(set1))
+# print(set1 >= set2)
+# # print(set1.issuperset(set2))
+# print(set1 >= set3)
+# # print(set1.issuperset(set3))
+
+# 创建字典的字面量语法
+scores = {'杨军': 92, '白元芳': 78, '狄仁杰': 82}
+print(scores)
+# 创建字典的构造器语法
+items1 = dict(one=1, two=2, three=3, four=4)
+# 通过zip函数将两个序列压成字典
+items2 = dict(zip(['a', 'b', 'c'], '123'))
+# 创建字典的推导式语法
+items3 = {num: num ** 2 for num in range(1, 10)}
+print(items1, items2, items3)
